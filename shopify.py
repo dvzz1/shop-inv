d0e479bd592735e39b566fe9b31a21a58c8ea5c5
dvzz1 <76510364+dvzz1@users.noreply.github.com>
2021-01-01 17:34:02 -0500

    Update shopify.py

--- shopify.py
+++ shopify.py
@@ -10,7 +10,7 @@ from shutil import copyfile
 
 collections = []
 DISCORD_HOOK = 'https://discord.com/api/webhooks/789275376487628831/7_Xom18raIJWVbd1JZY3Lh72Z5DrP8wA7yFqbqfS830kWVT5ZzkchBNYZh3-OHb3JNi7'
-url = 'hatclub.com'
+url = 'https://www.hatclub.com/'
 
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
